402862acc7be3480d39e9025c38802f6fdfc059f
Andreas Vogel <vogeandr@gmail.com>
2019-06-16 13:29:56 +0200

    Blockchain speichern mit Json in lokaler txt-Datei

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 from functools import reduce
 import hashlib as hl
 from collections import OrderedDict
+import json
 
 from hash_util import hash_string_256, hash_block
 
@@ -19,6 +20,43 @@ owner = 'Andreas'
 participants = {'Andreas'}
 
 # Functions ---------------------------------------------------
+
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        file_content = f.readlines()
+        global blockchain         # explizites global, damit die Funktion auf
+        global open_transactions  # auf die globalen Variable zugreift, anstatt locale zu erzeugen. 
+        blockchain = json.loads(file_content[0][:-1]) # [:-1] zum Weglassen von \n
+        # Aufgrund dessen das wir OrderedDict verwenden, muss hier eine Konvertierung erfolgen.
+        # da der String eines OrderedDict sich von einer normalen Liste unterscheidet. 
+        
+        updated_blockchain = []
+        for block in blockchain:
+            updated_block = {
+                'previous_hash': block['previous_hash'],
+                'index': block['index'],
+                'proof': block['proof'],
+                'transactions': [OrderedDict(
+                    [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block['transactions']]
+            }
+            updated_blockchain.append(updated_block)
+        blockchain = updated_blockchain
+        
+        open_transactions = json.loads(file_content[1])
+
+        #Auch hier, aufgrund von OrderedDict, muss eine Umwandlung stattfinden. 
+        updated_transactions = []
+        for tx in open_transactions:
+            updated_transaction = OrderedDict(
+                [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
+            updated_transactions.append(updated_transaction)
+        open_transactions = updated_transactions
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(json.dumps(blockchain))
+        f.write('\n')
+        f.write(json.dumps(open_transactions))
 
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
@@ -85,6 +123,7 @@ def add_transaction(recipient, sender = owner, amount = 1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -154,6 +193,8 @@ def verify_transactions():
     # return is_valid
 # Code --------------------------------------------------------
 
+load_data()
+
 waiting_for_input = True
 
 while waiting_for_input:
@@ -177,6 +218,7 @@ while waiting_for_input:
     elif user_choice == '2':
         if mine_block():
             open_transactions = []
+            save_data()
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
